2b7fa8d4fb59681372c0e5a1a2e51412b2c27122
Tsahi Atias <tsahi_atias@hotmail.com>
2021-08-05 09:00:04 +0300

    build project

--- SubmarinesGame/Program.cs
+++ SubmarinesGame/Program.cs
@@ -10,6 +10,22 @@ namespace SubmarinesGame
     {
         static void Main(string[] args)
         {
+            var util = new GameUtil();
+            util.SetBoard();
+
+            Console.WriteLine(util.Hit(0, 0)); // TODO: should result Miss
+            
+            Console.WriteLine(util.Hit(0, 0)); // TODO: should result Miss
+            Console.WriteLine(util.Hit(1, 0)); // TODO: should result Boom
+            Console.WriteLine(util.Hit(0, 3)); // TODO: should result Hit
+            Console.WriteLine(util.Hit(0, 4)); // TODO: should result Boom
+            Console.WriteLine(util.Hit(4, 1)); // TODO: should result Hit
+            Console.WriteLine(util.Hit(4, 2)); // TODO: should result Hit
+            Console.WriteLine(util.Hit(4, 3)); // TODO: should result Hit
+            Console.WriteLine(util.Hit(4, 4)); // TODO: should result Boom
+            Console.WriteLine(util.Hit(4, 0)); // TODO: should result Miss
+
+            Console.ReadLine();
         }
     }
 }
